4cc5d8f5debb70154ac797ce590008964a0cbce6
경이현 <wm0755123@naver.com>
2025-02-04 00:32:51 +0900

    2025-02-04 UsernamePasswordAuthenticationFilter 커스텀, authenticationManager까지

---
+++ src/main/java/hello/jwtpractice/dto/JoinDTO.java
@@ -0,0 +1,13 @@
+package hello.jwtpractice.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDTO {
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/hello/jwtpractice/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package hello.jwtpractice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+
+    public String username;
+
+    private String password;
+
+    private String role;
+
+}
